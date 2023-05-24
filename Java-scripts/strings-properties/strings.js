53f6ae5ee98bebad7c784587c0b9199866b740fd
alam-nazmul <alam.mdnazmul@outlook.com>
2023-05-25 01:14:40 +0600

    strings js file

---
+++ Java-scripts/strings-properties/strings.js
@@ -0,0 +1,12 @@
+const s = "Hello World"
+
+console.log(s.length)
+console.log(s.toUpperCase())
+console.log(s.toLowerCase())
+console.log(s.substring(0, 5).toUpperCase())
+
+console.log(s.split(" "))
+
+
+const p = "tech, computer, IT, code"
+console.log(p.split(" "))
