9360c396e5b795134eacb72d0da3412818d31189
alam-nazmul <alam.mdnazmul@outlook.com>
2023-05-25 00:25:05 +0600

    datatype js file

---
+++ Java-scripts/datatype/datatypes.js
@@ -0,0 +1,19 @@
+const Name = "Nazmul";
+const age = 31;
+const isCool = true;
+const rating = 4.5;
+const x = null;
+const y = undefined;
+let z
+
+console.log(typeof rating);
+console.log("My name is " + Name + "and my age is " + age)
+
+// Template Strings
+console.log(`My name is ${Name} and I am ${age} years old`)
+
+/// Define a string
+const hello = `My name is ${Name} and I am ${age} years old`
+console.log(hello)
+
+
