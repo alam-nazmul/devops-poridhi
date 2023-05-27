99cbce0082852811bf73180c4ccd1153c7773c2b
alam-nazmul <alam.mdnazmul@outlook.com>
2023-05-28 02:55:22 +0600

    JS file of functions

---
+++ Java-scripts/functions/function.js
@@ -0,0 +1,14 @@
+function addNums(num1 = 1, num2 = 2)
+{
+    return num1+num2
+}
+console.log(addNums(5, 10))
+
+
+
+
+function addNums(num1 = 1, num2 = 2)
+{
+    return num1+num2
+}
+console.log(addNums())
