2267d5cfc81d286598fa8e9bbc0b92b5fd69ee4f
alam-nazmul <alam.mdnazmul@outlook.com>
2023-06-11 23:36:55 +0600

    add and export a function

---
+++ React/vite-project/src/Education.jsx
@@ -0,0 +1,8 @@
+export function Education () {
+	return (
+		<div>
+		<h3>Education</h3>
+		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur sint deserunt, odit consectetur iure quia quasi, soluta eaque repellendus similique corporis? Iste maiores in enim ducimus deserunt distinctio asperiores aliquam!</p>
+	</div>
+	)
+}
