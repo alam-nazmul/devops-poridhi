8e3607564b43b8aed63c738fb8c877497093c72d
alam-nazmul <alam.mdnazmul@outlook.com>
2023-06-11 23:36:42 +0600

    add and export a function

---
+++ React/vite-project/src/AboutMe.jsx
@@ -0,0 +1,16 @@
+export function AboutMe () {
+	return (
+		<div>
+		<h3>About me</h3>
+		<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus id corporis quos pariatur quod iste harum, quidem reprehenderit et cupiditate, voluptatum labore sint optio, amet asperiores excepturi ipsum iusto autem!</p>
+	</div>
+	)
+}
+
+export function Random() {
+	return (
+		<div>
+			<p>I am a random function</p>
+		</div>
+	)
+}
