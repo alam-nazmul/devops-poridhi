96ba60f159eb000d0d2e0b16d278192727f26ce6
alam-nazmul <alam.mdnazmul@outlook.com>
2023-06-11 23:36:48 +0600

    add and export a function

---
+++ React/vite-project/src/ContactMe.jsx
@@ -0,0 +1,16 @@
+export function ContactMe () {
+	return (
+		<div>
+		<h3>Contact me</h3>
+		<from>
+			<label>Your Name: </label>
+			<input type="text" />
+			<br></br>
+			<label>Your Query</label>
+			<input type="date" />
+			<br></br>
+			<button>Submit</button>
+		</from>
+	</div>
+	)
+}
